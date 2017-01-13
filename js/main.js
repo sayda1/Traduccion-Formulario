ec41c2f039fbb84089321688723ebe2bb49db8ed
sayda <sayda>
2017-01-13 15:43:08 -0500

    traducion lista

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+document.getElementById("form-signin-heading").
+innerHTML ="Por favor inicia secion";
+
+document.getElementById("inputEmail").placeholder="correo electronico";
+
+document.getElementById("inputPassword").placeholder="contraseña";
+document.querySelector("span").innerHTML="recordar datos";
+document.querySelector("button").innerHTML="Iniciar Secion"
